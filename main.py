a09dc066dc97d3ef5bbb4c4b39d2ac6c17e51ecb
RunSabba <136928282+RunSabba@users.noreply.github.com>
2023-08-17 02:53:22 +0000

    Completed Currency Rate API

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-from flask import Flask
+#Building a basic currency exchange API
+
+#Flask framework module is always lowercase
+from flask import Flask, jsonify
+
+
+from bs4 import BeautifulSoup
+import requests
+
+#webscrape script to get the currency rates
+def get_currency(in_currency, out_currency):
+  url = f'https://www.x-rates.com/calculator/?from={in_currency}&to={out_currency}&amount=1'
+  content = requests.get(url).text
+  soup = BeautifulSoup(content, 'html.parser')
+  rate = soup.find("span", class_="ccOutputRslt").get_text()
+  rate = float(rate[:-4])
+  
+  return rate
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return '<h1>Abbas Currency Rate API</h1>  <p>Example URL:  /api/v1/usd-jpy<p>'
+  return '<h1>Abbas Currency Rate API</h1> <p>Example URL: /api/v1/usd-eur</p>'
+
+@app.route('/api/v1/<in_cur>-<out_cur>')
+def api(in_cur, out_cur):
+  rate = get_currency(in_cur, out_cur)
+  result_dictionary = {'input_currency':in_cur, 'output_currency':out_cur, 'rate':rate}
+  return jsonify(result_dictionary)
 
 app.run(host='0.0.0.0')
