3bc906fbb9ce2ac32a01e458c82c17854d5398f6
RunSabba <136928282+RunSabba@users.noreply.github.com>
2023-08-17 02:01:30 +0000

    Abbas Currency Rate API

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-#Script to get top-headlines from a specific county. Using Japan for an example
+from flask import Flask
 
-import requests
+app = Flask(__name__)
 
-def news(country,api_Key='6a2297b099574b1b9ed2bc269aa1d0d1'):
+@app.route('/')
+def home():
+  return '<h1>Abbas Currency Rate API</h1>  <p>Example URL:  /api/v1/usd-jpy<p>'
 
-  url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={api_Key}'
-  r = requests.get(url)
-  content = r.json()
-  articles = content["articles"]
-  results = []
-  for article in articles:
-    results.append(f"TITLE\n'{article['title']}, '\nTITLE\n',{article['description']}")
-  return results  
-  
-print(news(country="jp",api_Key="6a2297b099574b1b9ed2bc269aa1d0d1"))
-  
-  
-
-
-
-
-  
+app.run(host='0.0.0.0')
